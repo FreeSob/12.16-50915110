073a3ca88c59345597f3a7d9f6e8d9c5617bae7e
FREE <84310382+FreeSob@users.noreply.github.com>
2024-12-18 21:46:56 +0800

    update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,38 +4,44 @@
 #include <QFile>
 #include <QTextStream>
 #include <QMessageBox>
-
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 
-    // Connect save button to the saveToFile slot
+
     connect(ui->action_5, &QAction::triggered, this, &MainWindow::saveToFile);
 }
-
 MainWindow::~MainWindow()
 {
     delete ui;
 }
-
 void MainWindow::saveToFile()
 {
     QString fileName = QFileDialog::getSaveFileName(this, tr("Save File"), "", tr("Text Files (*.txt);;All Files (*)"));
 
-    if (fileName.isEmpty()) {
-        return; // User canceled the save dialog
-    }
 
-    QFile file(fileName);
-    if (!file.open(QIODevice::WriteOnly | QIODevice::Text)) {
-        QMessageBox::warning(this, tr("Error"), tr("Cannot open file for writing: %1").arg(file.errorString()));
+
+
+    if (fileName.isEmpty()) {
         return;
     }
 
+
+
+
+    QFile file(fileName);
+    if (!file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+        QMessageBox::warning(this, tr("Error"), tr("打不開: %1").arg(file.errorString()));
+        return;
+    }
+
+
     QTextStream out(&file);
-    out << ui->textEdit->toPlainText(); // Save the content of textEdit
+
+    out << ui->textEdit->toPlainText();
     file.close();
+
 
     QMessageBox::information(this, tr("Success"), tr("File saved successfully!"));
 }
